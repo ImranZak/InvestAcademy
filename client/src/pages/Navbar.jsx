e90a8e57e64cf22985d0677ceb495f8a02642bcd
amsyar <amsyarcyborgx@gmail.com>
2024-09-05 22:42:16 +0800

    chore: fixed Navbar.jsx

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ onDebug }) => {
+const Navbar = ({}) => {
   return (
     <AppBar 
       position="fixed"
@@ -23,6 +23,7 @@ const Navbar = ({ onDebug }) => {
           <Button color="inherit" component={Link} to="/learning-mode" sx={{ color: '#ffffff', fontWeight: 'bold' }}>Course</Button>
           <Button color="inherit" component={Link} to="/guided-trading" sx={{ color: '#ffffff', fontWeight: 'bold' }}>Guided Training</Button>
           <Button color="inherit" component={Link} to="/real-life-scenario" sx={{ color: '#ffffff', fontWeight: 'bold' }}>Real Life Scenario</Button>
+          <Button color="inherit" component={Link} to="/casestudies" sx={{ color: '#ffffff', fontWeight: 'bold' }}>Case Studies </Button>
           <Button color="inherit" component={Link} to="/aboutus" sx={{ color: '#ffffff', fontWeight: 'bold' }}>About Us</Button>
         </Box>
       </Toolbar>
